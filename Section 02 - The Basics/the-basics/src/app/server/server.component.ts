9d2b19adf5b6b136ea4b7bc097418966fd4ee17b
Steven <stevenxchung@gmail.com>
2018-08-27 18:56:34 -0400

    Add controller for server component

--- Section 02 - The Basics/the-basics/src/app/server/server.component.ts
+++ Section 02 - The Basics/the-basics/src/app/server/server.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.scss']
 })
 export class ServerComponent implements OnInit {
+  allowNewServer: boolean = false;
+  server: string = 'AWS';
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+  serverCreationStatus: string = 'No server was created!';
+  serverName: string = 'Test Server';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  ngOnInit() {}
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
