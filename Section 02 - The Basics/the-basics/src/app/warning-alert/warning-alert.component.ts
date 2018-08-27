bc071651fc84c3217a96be0f370ef2fed16ea805
Steven <stevenxchung@gmail.com>
2018-08-27 18:57:07 -0400

    Add warning alert component

---
+++ Section 02 - The Basics/the-basics/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.scss']
+})
+export class WarningAlertComponent implements OnInit {
+  alert = 'Warning!';
+
+  constructor() {}
+
+  ngOnInit() {}
+}
