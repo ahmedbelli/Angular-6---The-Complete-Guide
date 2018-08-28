546e7e0c4754bff3fe2404d424ca795c788c7a67
Steven <stevenxchung@gmail.com>
2018-08-28 18:56:42 -0400

    Add assignment methods to server component controller

--- Section 02 - The Basics/the-basics/src/app/server/server.component.ts
+++ Section 02 - The Basics/the-basics/src/app/server/server.component.ts
@@ -6,12 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.scss']
 })
 export class ServerComponent implements OnInit {
+  display: boolean = true;
   allowNewServer: boolean = false;
+  allowNewUser: boolean = false;
   server: string = 'AWS';
   serverId: number = 10;
   serverStatus: string = 'offline';
   serverCreationStatus: string = 'No server was created!';
   serverName: string = 'Test Server';
+  username: string = 'Steven';
+  loggedClicks: Array<any> = [];
+
+  public click = 0;
 
   constructor() {
     setTimeout(() => {
@@ -26,10 +32,26 @@ export class ServerComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  // Assignments begin here
+
+  resetUser() {
+    this.username = '';
+  }
+
+  toggleDisplay() {
+    return (this.display = !this.display);
+  }
+
+  logClicks() {
+    this.click += 1;
+    this.loggedClicks.push('This user is on click no. ' + this.click);
   }
 }
