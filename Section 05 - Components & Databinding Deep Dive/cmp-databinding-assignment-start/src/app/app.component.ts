eeeef428a03479f71e7f75bb74615d50569c6273
Steven <stevenxchung@gmail.com>
2018-09-04 20:34:28 -0400

    Add sub components and logic

--- Section 05 - Components & Databinding Deep Dive/cmp-databinding-assignment-start/src/app/app.component.ts
+++ Section 05 - Components & Databinding Deep Dive/cmp-databinding-assignment-start/src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers: Array<number> = [];
+  evenNumbers: Array<number> = [];
+
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 !== 0) {
+      this.oddNumbers.push(firedNumber);
+    } else {
+      this.evenNumbers.push(firedNumber);
+    }
+  }
 }
