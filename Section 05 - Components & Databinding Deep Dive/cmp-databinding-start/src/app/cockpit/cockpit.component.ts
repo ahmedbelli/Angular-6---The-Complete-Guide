3da416f7e34782ad30e4e464036773368839a747
Steven <stevenxchung@gmail.com>
2018-09-05 23:44:50 -0400

    Add spacing to app

--- Section 05 - Components & Databinding Deep Dive/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ Section 05 - Components & Databinding Deep Dive/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -18,11 +18,13 @@ export class CockpitComponent implements OnInit {
     serverName: string;
     serverContent: string;
   }>();
+
   @Output()
   blueprintCreated = new EventEmitter<{
     serverName: string;
     serverContent: string;
   }>();
+
   newServerName = '';
   newServerContent = '';
   @ViewChild('serverContentInput')
